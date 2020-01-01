4dc45fa32d297d0b13ed89890f07398631c36032
jesi-rgb <blograso@gmail.com>
2020-01-02 00:47:20 +0100

    palette showing up in the img itself

--- icon_generator.py
+++ icon_generator.py
@@ -4,59 +4,13 @@ import numpy as np
 from PIL import Image
 import colorsys as cs
 import math
+import random
 
 seed = np.random.randint(120498)
 np.random.seed(seed)
-
-def map_lerp(n, start1, stop1, start2, stop2):
-    return ((n-start1)/(stop1-start1))*(stop2-start2)+start2
+random.seed(seed)
 
 class Board:
-
-    colors = {
-        'white': np.array([255, 255, 255]),
-        'black': np.array([0, 0, 0]),
-        'fruit': np.array([255, 66, 80]),
-        'darkSand': np.array([214, 170, 75]),
-        'darkGreen': np.array([34, 139, 34]),
-        'lightSand': np.array([230, 210, 175]),
-        'green': np.array([119, 204, 65]),
-        'darkGreen': np.array([17, 74, 17]),
-        'snow': np.array([220, 220, 220]),
-        'mountain': np.array([139, 137, 137])
-    }
-
-    palette1 = [
-        np.array([142, 177, 199]),
-        #8EB1C7
-        np.array([176, 46, 12]),
-        #B02E0C
-        np.array([235, 69, 17]),
-        #EB4511
-        np.array([193, 191, 181]),
-        #C1BFB5
-        np.array([254, 253, 255])
-    ]
-
-    palette2 = [
-        np.array([240, 162, 2]),
-        #F0A202
-        np.array([241, 136, 5]),
-        #F18805
-        np.array([217, 93, 57]),
-        #D95D39
-        np.array([32, 44, 89]),
-        #202C59
-        np.array([88, 31, 24])
-    ]
-
-    palette3 = [
-        np.array([156, 255, 250]),
-        np.array([172, 243, 157]),
-        np.array([176, 197, 146]),
-        np.array([169, 124, 115]),
-        np.array([175, 62, 77])
-    ]
 
     board = None
     width = 0
@@ -71,13 +25,15 @@ class Board:
     
 
     def gen_random(self):
+        kernel = 3
+
         generations = 10 * np.random.randint(2, 21)
         print("Generations: ", generations)
-        kernel = 3
-        p = np.random.rand()
+        
+        p = random.uniform(0.3, 1)
         print("Initial probability dist: ", p)
-        aux_matrix = self.board.copy()
 
+        aux_matrix = self.board.copy()
         bool_matrix = np.random.choice(a=[False, True], size=(self.width, self.height), p=[p, 1-p])
         
         margin = np.random.randint(5, 16)
@@ -106,7 +62,7 @@ class Board:
 
             self.board = aux_matrix
 
-        board.colorize()
+        board.colorize(margin)
 
         
     def generate_colors(self):
@@ -114,15 +70,16 @@ class Board:
         
         random_hue = np.random.rand()
         random_saturation = np.random.rand()
-        # random_value = np.clip(np.random.rand() + 0.25, 0, 1)
-        # print("Value: ", random_value)
-        base_color = np.array([random_hue, random_saturation, 1])
+        random_value = random.uniform(0.25, 1)
+        
+        base_color = np.array([random_hue, random_saturation, random_value])
         colors.append(base_color)
         
         for _ in range(4):
-            new_hue = math.fmod(base_color[0] + np.random.rand(), 1)
-            new_sat = math.fmod(base_color[1] + np.random.rand(), 1)
-            col = np.array([new_hue, new_sat, 1])
+            new_hue = math.fmod(base_color[0] + random.uniform(-.35, .35), 1)
+            new_sat = math.fmod(base_color[1] + random.uniform(-.35, .35), 1)
+            new_val = math.fmod(base_color[2] + random.uniform(-.35, .35), 1)
+            col = np.array([new_hue, new_sat, new_val])
             
             colors.append(col)
 
@@ -134,20 +91,20 @@ class Board:
             
             output.append(rgb_color)
 
-        hue = lambda color: color[0]
-        output.sort(key=hue)
+        sort_criteria = lambda color: 0.2126 * color[0] + 0.7152 * color[1] + 0.0722 * color[2]
+        output.sort(key=sort_criteria)
+
         return output
 
 
-    def colorize(self):
+    def colorize(self, margin):
         pal = self.generate_colors()
-        print("Colors: ", pal)
 
        
         # reshaping
         new_matrix = np.zeros((self.width, self.height, 3), 'uint8')
         
-        new_matrix[self.board == 255] = self.colors['white']
+        new_matrix[self.board == 255] = [255, 255, 255]
         new_matrix[self.board == 0] = pal[0]
 
         self.board = new_matrix
@@ -155,14 +112,11 @@ class Board:
 
         aux_matrix = self.board
         kernel = 3
-        margin = np.random.randint(5, 16)
-
-        
 
         for i in range((self.width // margin) + kernel // 2, ((margin - 1) * self.width // margin) - kernel // 2):
             for j in range((self.height // margin) + kernel // 2, ((margin - 1) * self.height // margin) - kernel // 2):
 
-                if np.array_equiv(self.board[i, j], self.colors['white']):
+                if np.array_equiv(self.board[i, j], [255, 255, 255]):
                     val = noise.pnoise2(i, j, octaves=4, lacunarity=0.23, persistence=2)
 
                     if val > -0.5 and val <= -0.25:
@@ -175,6 +129,14 @@ class Board:
                         aux_matrix[i, j] = pal[4]
                         
         self.board = aux_matrix
+        self.board[0:5, 0:1] = [2, 2, 2]
+        self.board[0:1, 0:5] = [2, 2, 2]
+        self.board[0:5, 0:5] = pal[0]
+        self.board[5:10, 0:5] = pal[1]
+        self.board[10:15, 0:5] = pal[2]
+        self.board[15:20, 0:5] = pal[3]
+        self.board[20:25, 0:5] = pal[4]
+
 
 
     def display(self):
@@ -190,3 +152,4 @@ if __name__ == "__main__":
     board = Board(100, 100)
     board.gen_random()
     board.display()
+    print("\n***Program finished***\n\n\n")
